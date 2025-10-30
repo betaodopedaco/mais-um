a949926018a7c44eceaa86c31f380a9b2a83119e
nicolas emanuel <brancosupremo12@gmail.com>
2025-10-29 22:12:24 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Tradutor IA - Funcionando',
+  description: 'Finalmente caralho',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
